fdaf8914f46162f3684b0589491bb8c8a17f131f
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-11-13 15:40:36 +0000

    tests/u: test_hostuserutil fix test and convert to pytest

--- tests/unit/test_hostuserutil.py
+++ tests/unit/test_hostuserutil.py
@@ -13,50 +13,56 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Test aspects of "cylc.flow.hostuserutil"."""
 
 import os
-import unittest
+
+import pytest
 
 from cylc.flow.hostuserutil import (
-    get_fqdn_by_host, get_host, get_user, get_user_home, is_remote_host,
-    is_remote_user)
+    get_fqdn_by_host,
+    get_host,
+    get_user,
+    get_user_home,
+    is_remote_host,
+    is_remote_user
+)
 
 
-class TestHostUserUtil(unittest.TestCase):
-    """Test aspects of "cylc.flow.hostuserutil"."""
-
-    def test_is_remote_user_on_current_user(self):
-        """is_remote_user with current user."""
-        self.assertFalse(is_remote_user(None))
-        self.assertFalse(is_remote_user(os.getenv('USER')))
-
-    def test_is_remote_host_on_localhost(self):
-        """is_remote_host with localhost."""
-        self.assertFalse(is_remote_host(None))
-        self.assertFalse(is_remote_host('localhost'))
-        self.assertFalse(is_remote_host(os.getenv('HOSTNAME')))
-        self.assertFalse(is_remote_host(get_host()))
-
-    def test_get_fqdn_by_host_on_bad_host(self):
-        """get_fqdn_by_host bad host."""
-        bad_host = 'nosuchhost.nosuchdomain.org'
-        with self.assertRaisesRegex(
-                IOError,
-                r"(\[Errno -2\] Name or service|"
-                r"\[Errno 8\] nodename nor servname provided, or)"
-                r" not known: '{}'".format(bad_host)) as ctx:
-            get_fqdn_by_host(bad_host)
-
-        self.assertEqual(ctx.exception.filename, bad_host)
-
-    def test_get_user(self):
-        """get_user."""
-        self.assertEqual(os.getenv('USER'), get_user())
-
-    def test_get_user_home(self):
-        """get_user_home."""
-        self.assertEqual(os.getenv('HOME'), get_user_home())
+def test_is_remote_user_on_current_user():
+    """is_remote_user with current user."""
+    assert not is_remote_user(None)
+    assert not is_remote_user(os.getenv('USER'))
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_is_remote_host_on_localhost():
+    """is_remote_host with localhost."""
+    assert not is_remote_host(None)
+    assert not is_remote_host('localhost')
+    assert not is_remote_host(os.getenv('HOSTNAME'))
+    assert not is_remote_host(get_host())
+
+
+def test_get_fqdn_by_host_on_bad_host():
+    """get_fqdn_by_host bad host."""
+    bad_host = 'nosuchhost.nosuchdomain.org'
+    with pytest.raises(
+        (IOError, OSError),
+        match=(
+            r"(\[Errno -2\] Name or service|"
+            r"\[Errno 8\] nodename nor servname provided, or)"
+            rf" not known: '{bad_host}'"
+        )
+    ) as ctx:
+        get_fqdn_by_host(bad_host)
+    assert ctx.value.filename == bad_host
+
+
+def test_get_user():
+    """get_user."""
+    assert os.getenv('USER') == get_user()
+
+
+def test_get_user_home():
+    """get_user_home."""
+    assert os.getenv('HOME') == get_user_home()
